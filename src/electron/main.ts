d1b1aec11bcc008d82040ff6b2091f819731f354
Anton Ivanov <74968880+ANTON-IVANOVICH@users.noreply.github.com>
2024-12-30 22:59:09 +0300

    init & starter setup

---
+++ src/electron/main.ts
@@ -0,0 +1,14 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+
+app.on("ready", () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+        },
+    });
+
+    win.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
+})
